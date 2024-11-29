accf653056212a46abb1f52c1c28eeea8d9d37a7
Myint Myat Pyae Sone <myintmyatps664@gmail.com>
2024-11-29 23:12:02 +0630

    Reviews controller and its apiPostReview method was created

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import MoviesController from '../movies.controllers.js';
+import MoviesController from '../controllers/movies.controllers.js';
+import ReviewsController from '../controllers/reviews.controllers.js';
 
 const router = express.Router()
 
 router.route('/').get(MoviesController.apiGetMovies);
+
+router.route("/review").post(ReviewsController.apiPostReview).put(ReviewsController.apiUpdateReview).delete(ReviewsController.apiDeleteReview)
 
 export default router;
