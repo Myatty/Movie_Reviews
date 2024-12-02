13aed811f525fc15771b15eee59ab5ddc62ed8e1
Myatty <myintmyatps664@gmail.com>
2024-12-02 11:04:39 +0630

    Add and configure frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
