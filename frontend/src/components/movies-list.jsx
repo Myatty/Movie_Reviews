cfd7601b3e6ff90f95307cab64718cb2a32fc326
Myatty <myintmyatps664@gmail.com>
2024-12-04 00:20:54 +0630

    Add states in movies-list

--- frontend/src/components/movies-list.jsx
+++ frontend/src/components/movies-list.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import MovieDataService from "../services/movies";
+import Link from "react-router-dom";
 
-function MoviesList(){
-    return(
-        <div className="App">
-            Movie List
-        </div>
-    )
+const MoviesList = props => {
+    
+    const [movies,setMovies] = useState([])
+    const [searchTitle, setSearchTitle] = useState("")
+    const [searchRating, setSearchRating] = useState("")
+    const [ratings, setRatings] = useState(["All Ratings"])
 }
 
 export default MoviesList;
